9855082abb5aec1bc5f2d4608158a5b655567808
Sviatoslav Abakumov <sviatoslav.abakumov@ivelum.com>
2019-12-04 17:57:35 +0300

    Fix getting the API key

--- test/Cub/Test/TestCase.php
+++ test/Cub/Test/TestCase.php
@@ -4,12 +4,11 @@ class Cub_Test_TestCase extends \PHPUnit\Framework\TestCase
 
 {
     const API_URL = 'https://id.lexipol.com/v1/';
-    const API_KEY = getenv('INTEGRATION_TESTS_SECRET_KEY');
     public $details;
     public $credentials;
     public function setUp()
     {
-        Cub_Config::$api_key = self::API_KEY;
+        Cub_Config::$api_key = getenv('INTEGRATION_TESTS_SECRET_KEY');
         Cub_Config::$api_url = self::API_URL;
         $this->details = array(
             'original_username' => 'ivelum',
